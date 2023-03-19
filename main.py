ca66651cdc51efbb28fc1e3ee14e9357b532f9eb
Aakash-Sudersan-Selvaganapathy <126450228+Aakash-Sudersan-Selvaganapathy@users.noreply.github.com>
2023-03-19 15:31:05 +0800

    UI changes (#2)
    Co-authored-by: Aakash <aakash.adithya@gmail.com>

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ app.mount("/static",StaticFiles(directory="static",html=True),name="static")
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+
+@app.get("/login/login.html")
+def home(request: Request):
+    return templates.TemplateResponse("login/login.html", {"request": request})
+
+@app.get("/login/register.html")
+def home(request: Request):
+    return templates.TemplateResponse("login/register.html", {"request": request})
