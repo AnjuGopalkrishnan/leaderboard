3dcb3667622b629e1fa13f08cdb409ea53913643
Ritu Raj <rituraj@Ritus-MacBook-Pro.local>
2023-03-23 07:35:50 +0800

    Add login page apis

---
+++ infra/db.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime,BigInteger,ForeignKey,inspect,MetaData,Table
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('postgresql://rituraj:@localhost:5432/leaderboard', echo=True)
+metadata = MetaData()
+connection = engine.connect()
+Base = declarative_base()
+
+
+competitions = Table(
+   'competitions', metadata,
+   Column('c_id', BigInteger, primary_key = True),
+   Column('description', String),
+   Column('schema', String),
+   Column('solution', String),
+   Column('host_user_id', Integer,primary_key = True),
+   Column('due_date', DateTime,primary_key = True),
+)
+
+class Competitions(Base):
+   __table__ = competitions
+
+
+submissions = Table(
+   'submissions', metadata,
+   Column('c_id', Integer, primary_key = True),
+   Column('user_id', Integer, primary_key = True),
+   Column('submission', String),
+   Column('score', Integer),
+   Column('timestamp', DateTime,primary_key = True),
+)
+
+class Submissions(Base):
+   __table__ = submissions
+
+def GetDBConnection():
+    return connection
--- main.py
+++ main.py
@@ -1,14 +1,105 @@
-from fastapi import FastAPI, Request
- 
+from fastapi import FastAPI, Request, HTTPException
 from starlette.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
- 
+from sqlalchemy import text
+import infra.db
+from lib import jwt
+
+import lib.authenticate
+from infra import models
+
 templates = Jinja2Templates(directory="templates")
- 
+
 app = FastAPI()
- 
-app.mount("/static",StaticFiles(directory="static",html=True),name="static")
- 
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/v1/users/register")
+def register(user: models.User):
+    print(user)
+    hashPwd = lib.authenticate.get_password_hash(user.password)
+    values = {
+        'username': user.username,
+        'password': hashPwd
+    }
+    query = text("INSERT INTO users(user_name, password) VALUES(:username, :password)")
+    with infra.db.engine.begin() as conn:
+        res = conn.execute(query, values)
+
+    if res:
+        return {
+            "username": user.username,
+            "register_success": True
+        }
+    return {
+        "register_success": False
+    }
+
+
+@app.post("/v1/user/login")
+def login(user: models.User):
+    values = {
+        'username': user.username,
+    }
+    query = text("SELECT * FROM users WHERE user_name = :username LIMIT 1")
+    with infra.db.engine.begin() as conn:
+        res = conn.execute(query, values)
+
+    if not res:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+        )
+    for row in res:
+        if not lib.authenticate.verify_password(user.password, row[1]):
+            raise HTTPException(
+                status_code=401,
+                detail="Incorrect username or password",
+            )
+    access_token = jwt.create_access_token(payload={"username":user.username})
+    return {"access_token":access_token}
+
+
+@app.get("/v1/user/{id}/hosted")
+def hosted(id):
+    query = infra.db.competitions.select().where(infra.db.competitions.c.host_user_id == id)
+    with infra.db.engine.begin() as conn:
+        res = conn.execute(query)
+
+    ansDict = []
+    for idx, row in enumerate(res):
+        tempDict = {}
+        for idx, key in enumerate(res.keys()):
+            tempDict[key] = row[idx]
+        ansDict.append(tempDict)
+
+    return ansDict
+
+
+@app.get("/v1/user/{id}/competitions")
+def getcompetitions(id):
+    query = infra.db.submissions.select().where(infra.db.submissions.c.user_id == id)
+    with infra.db.engine.begin() as conn:
+        res = conn.execute(query)
+
+    compIds = []
+    for idx, row in enumerate(res):
+        compIds.append(row[0])
+
+    query2 = infra.db.competitions.select().where(infra.db.competitions.c.c_id.in_(compIds))
+    with infra.db.engine.begin() as conn:
+        res = conn.execute(query2)
+
+    ansDict = []
+    for idx, row in enumerate(res):
+        tempDict = {}
+        for idx, key in enumerate(res.keys()):
+            tempDict[key] = row[idx]
+        ansDict.append(tempDict)
+
+    return ansDict
