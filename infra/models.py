658f69bdb7f4d693b97e2da24554bd37e68d2606
anand-subu <anand.subu10@gmail.com>
2023-03-26 15:07:55 +0530

    Add pydantic models for parsing requests for competition and submission requests

--- infra/models.py
+++ infra/models.py
@@ -1,6 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
     password: str
     username: str
+
+
+class Competitions(BaseModel):
+    title: str
+    description: str
+    schema_file: str = Field(..., alias="schema")
+    solution: str
+    host_user_id: int
+
+
+class Submission(BaseModel):
+    c_id: int
+    user_id: int
+    submission: str
